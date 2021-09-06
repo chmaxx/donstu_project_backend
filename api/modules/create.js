d49866a0928bbe1dd1d4a6165501b7c267a60b7b
Artyom <m.zaharenkov@mail.ru>
2021-09-06 21:54:02 +0300

    Небольшое пояснение тестового модуля
    Слегка пояснил причину существования этого модуля (и слегка дописал его, чисто для наглядности)

--- api/modules/create.js
+++ api/modules/create.js
@@ -1,6 +1,15 @@
+/*
+	Это тестовый модуль - здесь я просто проверяю работу приложения 
+	На данном этапе мы можем отправить POST-запрос на приложение <сайт>:8000/create-post
+	с флагом x-www-form-urlencoded и определенными данными в Body, а затем получить их здесь
+	в отформатированном виде (req.body)
+*/
 module.exports = function(server, db) {
 	server.post('/create-post', (req, res) => {
+		console.log('Got POST!')
 		console.log(req.body)
-		res.send('Hello')
+
+		let test_val = req.test_value ? req.test_value : 'Nothing in test_value...'
+		res.send('Got your POST! ' + test_val)
 	});
 };
