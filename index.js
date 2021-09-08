168fe7ce6f0d42ef4d7ba5ad595c43039cc79aa9
whitekeep <vip.fox.fox.3@gmail.com>
2021-09-08 14:41:06 +0300

    изменение логов

--- index.js
+++ index.js
@@ -22,14 +22,14 @@ db_connect(config.db_settings.url);
 
 
 // Подгружаем ssl сертификат и ключ
-// В случае ошибки - отменяем запуск сервера
+// В случае ошибки при 443 порте - отменяем запуск сервера
 try {
   var cert_opt = {
     key: fs.readFileSync(config.key_path),
     cert: fs.readFileSync(config.cert_path)
   };
 } catch (err) {
-  console.log('Ошибка при загрузке ssl сертификата и ключа!')
+  console.log('SSL сертификат и ключ не загружен. Работа по HTTPS невозможна')
   if (config.port == 443) {
     console.log('Отмена запуска сервера на порту 443. Смените порт или укажите правильеый путь к ssl сертификату и ключу')
     return
@@ -41,7 +41,7 @@ try {
 if (config.port == 443) {
   var https = require('https').createServer(cert_opt,app);
   https.listen(config.port, function () {
-    console.log('HTTP: Начинаем прослушку порта ', config.port);
+    console.log('HTTPS: Начинаем прослушку порта ', config.port);
   });
 } else {
   // Начинаем прослушку не на https порте
