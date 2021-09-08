6a82e430b9f62f6b3abebf46b72ff590fc66ce5d
Artyom <m.zaharenkov@mail.ru>
2021-09-08 17:21:11 +0300

    Небольшие стилистические изменения
    по совету Селезнева переменные вне функций буду объявлять через let (проверить, повлияет ли это на продакшн-версию)

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-var app = require('express')();
-var fs = require('fs')
+let app = require('express')();
+let fs = require('fs')
 
-
-//Подгружаем конфиг 
-//Если не удается подгрузить - отменяем запуск сервера
-var config 
+// Подгружаем конфиг 
+// Если не удается подгрузить - отменяем запуск сервера
+let config 
 try {
   config = require('./config/config.json');
   global.api_config = config;
@@ -12,7 +11,6 @@ try {
   console.error('Не удается загрузить конфигурационный файл!');
   return;
 }
-
 
 // Подгружаем маршруты
 require('./api/routes')(app, config);
@@ -22,26 +20,26 @@ app.use(require('./db'))
 
 // Подгружаем ssl сертификат и ключ
 // В случае ошибки при 443 порте - отменяем запуск сервера
+let isHTTPS = config.port == 443
 try {
-  var cert_opt = {
+  let cert_opt = {
     key: fs.readFileSync(config.key_path),
     cert: fs.readFileSync(config.cert_path)
   };
 } catch (err) {
   console.log('SSL сертификат и ключ не загружен. Работа по HTTPS невозможна');
-  if (config.port == 443) {
+  if (isHTTPS) {
     console.log('Ошибка при загрузке ssl сертификата и ключа!');
-    console.log('Отмена запуска сервера на порту 443. Смените порт или укажите правильеый путь к ssl сертификату и ключу');
+    console.log('Отмена запуска сервера на порту 443. Смените порт или укажите правильный путь к ssl сертификату и ключу');
     return;
   } else {
     console.log('SSL сертификат и ключ не загружены!');
   }
 }
 
-
 // проверка порта для запуска нужного протокола
-if (config.port == 443) {
-  var https = require('https').createServer(cert_opt,app);
+if (isHTTPS) {
+  let https = require('https').createServer(cert_opt, app);
   https.listen(config.port, function () {
     console.log('HTTPS: Начинаем прослушку порта ', config.port);
   });
