f40b507894c275e65c5dbae1334b6a4dcd02b6c2
Artyom <m.zaharenkov@mail.ru>
2021-09-08 19:52:24 +0300

    Revert "Наброски маршрута "Статьи""
    This reverts commit e6baa390f269fe83bb0ee42aed743313999ab0c9.

---
+++ api/routes/posts.js
@@ -0,0 +1,29 @@
+/* Тестовый маршрут Posts 
+ * Запросы происходят по пути:
+ *
+ * http://<сайт>/v1/posts/
+ *
+ * Далее все добавляемые маршруты через router будут автоматически отнесенены к v1/posts/
+ * 
+ * Например: 
+ *
+ * 		router.route('/create')
+ *			.get(console.log)
+ *
+ * ^^^ при такой инициализации маршрута полный путь до него будет:
+ * http://<сайт>/v1/posts/create
+ *
+ * при этом стоит обратить внимание, что v1 здесь - версия API, указанная в конфиге
+ */
+
+ // "/" тут относительно названия файла маршрута
+const {Router} = require('express');
+const router = Router();
+
+router.route('/')
+	.get((req, res) => {
+		console.log('GET /v1/posts/')
+		res.send(req.query);
+	})
+
+module.exports = router
