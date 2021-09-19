f82d769cff21b185ab04adc6782478ef0fc8e71b
Artyom <m.zaharenkov@mail.ru>
2021-09-19 12:21:02 +0300

    Небольшой рефакторинг статей
    Изменил схему разделения (теперь она идентична тому, что находится в маршруте пользователей), убрал некоторые лишние комментарии (и буду убирать дальше)

--- features/articles/controller.js
+++ features/articles/controller.js
@@ -1,108 +1,171 @@
-/* Все, что нужно для работы со статьями 
- * Я решил оставить обработку входных данных в роутах, чтобы эти функции в будущем можно было
- * использовать где-то за границами роутов 
- */ 
+const {isJSON}  = require('../utils');
+const ArticleService = require('./service');
 
-const Article   = require('./schema');
+class ArticleController {
+    async get(req, res, next) {
+        try {
+            let filter = {};
+           
+            /* Убираем из выдачи все архивированные статьи
+            * Поскольку функционал архивирования добавлен недавно, у некоторых статей в базе данных может не быть этого поля,
+            * поэтому стоит добавить флаг $ne
+            */
+            filter.is_archived = {$ne: true}; 
 
-/* Все функции будут возвращать массив, где первое значение ВСЕГДА html-статус, а последующие 
- * будут уникальны для каждой функции (но зачастую это будут обьекты, которые будут передаваться сразу клиенту)
- */
-
-/* Для реализации всей этой балды мне пришлось прибегнуть к использованию async/await (и try..catch)
- * Обязательно ли это?
- */
-
-// Получение статей
-module.exports.get = async (filters) => {
-	let code, response_contents;
-
-  try {
-    response_contents = await Article.find(filters);
-    code = 200;
-  } 
-  catch(err) {
-    response_contents = {msg: err.message};
-    code = 500; 
-  };
-
-  return [code, response_contents];
-}
-
-// Создание статьи
-module.exports.create = async (header, author, contents, tags, content_type) => {
-  let code, response_contents;
-	let currentDate = new Date();
-
-  const new_article = new Article({
-    header            : header, 
-    author_id         : author, 
-    contents          : contents, 
-    tags              : tags,
-    create_time       : currentDate, 
-    last_update_time  : currentDate
-  });	
-
-  try {
-    let savedArticle = await new_article.save(); 
+            // Пользовательские фильтры
+            if (req.body.filters && isJSON(req.body.filters)) {
+                const req_filters = JSON.parse(req.body.filters);
     
-    response_contents = {msg: 'Статья успешно добавлена!', created_article_id: savedArticle._id};
-    code = 200;
-  } 
-  catch (err) {
-    response_contents = {msg: err.message};
-    code = 500;
-  };
+                if (req_filters.author_id) filter.author_id = req_filters.author_id;
+                if (req_filters.tags) filter.tags = req_filters.tags;
+                if (req_filters.content_type) filter.content_type = content_type;
+                
+                // Временные рамки (создание)
+                // Должно быть списком с двумя значениями: начало-конец (по умолчанию конец - текущее время)
+                if (req_filters.date_interval) {
+                    filter.create_time = {
+                    $gte: req_filters.date_interval[0], 
+                    $lte: req_filters.date_interval[1] ? req_filters.date_interval[1] : new Date()};
+                }
+            }
 
-  return [code, response_contents];
+            const [code, response_contents] = await ArticleService.get(filter);
+            return res.status(code).json(response_contents);            
+        } catch (e) {
+
+        }
+    }
+
+    async add(req, res, next) {
+        try {
+            if (api_config && !api_config.db_settings.enabled) {
+                return res.status(500).json({msg: 'База данных отключена!'});      
+            }
+            
+            if (!req.body.name) { 
+                return res.status(400).json({msg: 'Необходимо ввести название статьи!'});  
+            }
+            
+            // Это необязательный аргумент, поэтому ничего плохого не произойдет, если мы передадим это в функцию
+            var content_type;
+             
+            if (req.body.content_type && typeof req.body.content_type === 'string') 
+                content_type = req.body.content_type;
+            
+            const [code, response_contents] = await ArticleService.create(
+                req.body.name, // header
+                req.body.author || 'system', // author
+                parseContents(req.body.contents), // contents 
+                parseTags(req.body.tags), // tags
+                content_type // content_type
+            );
+            
+            res.status(code).json(response_contents);
+        } catch (e) {
+            next(e);
+        }
+    }
+
+    async archive(req, res, next) {
+        try {
+            if (api_config && !api_config.db_settings.enabled) {
+                return res.status(500).json({msg: 'База данных отключена!'});
+            }
+            
+            if (!req.body.article_id) {
+                return res.status(400).json({msg: 'Необходимо ввести ID статьи!'}); 
+            }
+            
+            var [code, response_contents] = await ArticleService.archive(req.body.article_id);
+            res.status(code).json(response_contents);
+        } catch (e) {
+            next(e);
+        }
+    }
+
+    async unarchive(req, res, next) {
+        try {
+            if (api_config && !api_config.db_settings.enabled) {
+                return res.status(500).json({msg: 'База данных отключена!'});    
+            }  
+            
+            if (!req.body.article_id) {
+                return res.status(400).json({msg: 'Необходимо ввести ID статьи!'});  
+            }
+            
+            var [code, response_contents] = await ArticleService.unarchive(req.body.article_id);
+            res.status(code).json(response_contents);
+        } catch (e) {
+            next(e);
+        }
+    }
 }
 
-// Архивирование статьи
-module.exports.archive = async (article_id) => {
-  let code, response_contents;
 
-  try {
-    let articleToArchive = await Article.findOne({_id: article_id}); 
+/* Функционал парсинга контента, получаемого от пользователей
+ * При любой неудаче возвращает fallbackContent
+ */
+const fallbackContent = [
+    {
+      type: 'markdown',
+      value: 'У данной статьи нет содержимого!'
+    }
+]; 
+function parseContents(contentData, fallbackTo = fallbackContent) {
+    if (!isJSON(contentData)) return fallbackContent; 
+  
+    var json_content;
+    try {
+        json_content = JSON.parse(contentData);
+    } catch (e) {
+        json_content = fallbackContent;
+    }
+  
+    // Если после блока try-catch не удалось спарсить аргумент
+    // (при таком сценарии fallbackContent и json_content - одно и тоже)
+    if (fallbackContent == json_content) return fallbackContent;
 
-    // Единственный известный мне способ принудительно выйти из try..catch, сохраняя нужный мне функционал - вызвать ошибку
-    // но корректно ли в данном случае вызывать именно TypeError? 
-    if (articleToArchive.is_archived) throw new TypeError('Статья уже заархивирована!');
-
-    articleToArchive.is_archived = true; 
-    articleToArchive.save(); 
-
-    code = 200;
-    response_contents = {msg: 'Статья успешно архивирована!'};
-  } 
-  catch(err) {
-    code = err.name === 'TypeError' ? 400 : 500; 
-    response_contents = {msg: err.message};
-  };
-
-  return [code, response_contents];
+    if (json_content.length == 0) return fallbackContent;
+  
+    for (var i = 0; i < json_content.length; i++) {
+      var content_row = contentData[i];
+      // Если в одной из ячеек с контентом не указан тип контента - удаляем ее 
+      if (!content_row.type) {
+        delete(contentData[i]);
+        continue;
+      };
+    };
+  
+    return json_content;
 }
+  
+/* Функционал парсинга тегов, получаемых от пользователей
+ * При любой неудаче возвращает fallbackTags
+ */
+const fallbackTags = ['Ошибка при обработке']
+function parseTags(tagsData, fallbackTo = fallbackTags) {
+    if (!isJSON(tagsData)) return fallbackTags; 
+  
+    var json_tags; 
+    try {
+        json_tags = JSON.parse(tagsData);
+    } catch (e) {
+        json_tags = fallbackTags;
+    }
+  
+    // Если после блока try-catch не удалось спарсить аргумент
+    // (при таком сценарии fallbackTags и json_tags - одно и тоже)
+    if (json_tags == fallbackTags) return fallbackTags; 
+  
+    if (json_tags.length == 0) return fallbackTags;
+  
+    // Приводим все теги к типу String;
+    for (var i = 0; i < json_tags.length; i++) {
+      var tag = json_tags[i];
+      if (typeof tag !== 'string') json_tags[i] = tag.toString();
+    }
+  
+    return json_tags;
+}  
 
-// Удаление статьи из архива
-module.exports.unarchive = async (article_id) => {
-  let code, response_contents; 
-
-  try {
-    let archivedArticle = await Article.findOne({_id: article_id});
-
-    // Статья не заархивирована
-    // проблема TypeError обсуждена выше 
-    if (archivedArticle.is_archived != true) throw new TypeError('Данной статьи нет в архиве!');
-
-    archivedArticle.is_archived = false; 
-    archivedArticle.save();    
-
-    code = 200;
-    response_contents = {msg: 'Статья успешно удалена из архива!'};
-  }
-  catch (err) {
-    code = err.name === 'TypeError' ? 400 : 500; 
-    response_contents = {msg: err.message};
-  }
-
-  return [code, response_contents];
-}
+module.exports = new ArticleController;
