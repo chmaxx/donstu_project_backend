ca9a3b7e31e98f64a9a96ef18d6ffd05a0804306
whitekeep <vip.fox.fox.3@gmail.com>
2021-10-27 13:43:51 +0300

    использование нового логгера ошибок

--- middlewares/ErrorCatcher/index.js
+++ middlewares/ErrorCatcher/index.js
@@ -1,7 +1,12 @@
 const ApiError = require('../ApiErrorException');
 
+// подключаем логгер ErrorCatcher
+config = require('../../config/config.json');
+const Logger = require('../Logger');
+const log = new Logger(config.logger, 'ErrorCatcher');
+
 module.exports = (err, req, res, next) => {
-  console.log(err);
+  log.error(err);
   if (err instanceof ApiError) {
     return res
       .status(err.code)
