9f8eb2996d76356ce1a3791a6e89cc895402db36
whitekeep <vip.fox.fox.3@gmail.com>
2021-10-27 18:12:54 +0300

    Add config validator
    Валидация конфига по json схеме

--- middlewares/loadConfig/index.js
+++ middlewares/loadConfig/index.js
@@ -1,7 +1,15 @@
+const Ajv = require('ajv');
+const ajv = new Ajv();
+
 function loadConfig() {
   // стандартный конфиг
   try {
     var config_default = require('../../config/config_default.json');
+    var validate = ajv.compile(require('../../schemas/config_schema.json'));
+    if (!validate(config_default)) {
+      console.log(validate.errors);
+      return 0;
+    }
   } catch (err) {
     console.log(err);
     return;
@@ -13,15 +21,18 @@ function loadConfig() {
 
   try {
     var config = require('../../config/config.json');
-    log.info('Конфиг загружен');
-    //todo: проверка валидности значений в отдельности по схеме
+    if (!validate(config)) {
+      throw validate.errors;
+    } else {
+      log.info('Конфигурация загружена');
+    }
   } catch (err) {
     log.info('Не удается загрузить конфигурационный файл');
     log.error(err);
     log.info('Загрузка конфигурации по умолчанию');
     config = config_default;
   }
-
+  //todo: остановка приложения при ломанном дефолтном конфиге
   return config;
 }
 
