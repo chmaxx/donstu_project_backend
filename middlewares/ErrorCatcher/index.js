cdc3ca0bd5d00f5b981b0f23ff8763680fe16ef6
Maksim <m.zaharenkov@mail.ru>
2021-11-21 00:24:17 +0300

    Использование встроенной функции
    Почему-то до этого мы не использовали встроенную функцию JsonFormat и форматировали вручную (и парой строчек ниже использовали эту же функцию для других ошибок)

--- middlewares/ErrorCatcher/index.js
+++ middlewares/ErrorCatcher/index.js
@@ -7,14 +7,10 @@ const log = new Logger(api_config.logger, 'ErrorCatcher');
 module.exports = (err, req, res, next) => {
   log.error(err);
   if (err instanceof ApiError) {
-    return res
-      .status(err.code)
-      .json({ message: err.message, errors: err.errors });
+    return res.status(err.code).json(ApiError.JsonFormat(err));
   }
 
   const UnexpectedError = new ApiError();
 
-  return res
-    .status(UnexpectedError.code)
-    .json(ApiError.JsonFormat(UnexpectedError));
+  return res.status(UnexpectedError.code).json(ApiError.JsonFormat(UnexpectedError));
 };
