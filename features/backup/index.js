1762871729484b07eb144f24974602297a4c0728
whitekeep <vip.fox.fox.3@gmail.com>
2021-12-22 20:40:11 +0300

    вырезал backup

--- features/backup/index.js
+++
@@ -1,5 +0,0 @@
-var apiRoutePath = '/' + API_CONFIG.api_version + '/backup';
-
-module.exports = (app) => {
-  app.use(apiRoutePath, require('./route'));
-};
--- features/backup/route.js
+++
@@ -1,8 +0,0 @@
-const { Router } = require('express');
-const router = Router();
-const BackupController = require('./controller');
-
-router.get('/list', BackupController.getlist);
-router.get('/getbackup/:id?', BackupController.getbackup);
-
-module.exports = router;
